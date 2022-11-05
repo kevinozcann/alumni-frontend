eaa67882393f9c5c60553f56308675d818b3256b
Simsek Mert <simsek97@gmail.com>
2022-11-04 21:15:00 -0400

    Update next config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  i18n: {
+    locales: ["en-US", "tr-TR"],
+    defaultLocale: "en-US",
+  },
   reactStrictMode: true,
   swcMinify: true,
+  output: "standalone",
 };
 
 module.exports = nextConfig;
