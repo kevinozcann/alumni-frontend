6d536d643a1a2803a42e9759f1afe626ade3ab23
Simsek Mert <simsek97@gmail.com>
2022-12-22 15:31:59 -0500

    Update dependencies for lambda

--- amplify/backend/function/alumnifrontend88c8d6e7PostConfirmation/src/custom.js
+++ amplify/backend/function/alumnifrontend88c8d6e7PostConfirmation/src/custom.js
@@ -1,47 +1,81 @@
-var AWS = require('aws-sdk');
-var ddb = new AWS.DynamoDB({ apiVersion: '2012-10-08' });
+const { Sha256 } = require('@aws-crypto/sha256-js');
+const { defaultProvider } = require('@aws-sdk/credential-provider-node');
+const { SignatureV4 } = require('@aws-sdk/signature-v4');
+const { HttpRequest } = require('@aws-sdk/protocol-http');
+const { default: fetch, Request } = require('node-fetch');
+
+const GRAPHQL_ENDPOINT = process.env.API_AMPLIFYTRIPSPLANNER_GRAPHQLAPIENDPOINTOUTPUT;
+const AWS_REGION = process.env.AWS_REGION || 'us-east-1';
+
+const query = /* GraphQL */ `
+  mutation createUser($email: String!, $name: String!, $family_name: String!, $owner: String!) {
+    createUser(input: { email: $email, name: $name, family_name: $family_name, owner: $owner }) {
+      id
+      email
+    }
+  }
+`;
 
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 exports.handler = async (event, context) => {
-  console.log(event);
+  console.log(`Event: ${JSON.stringify(event)}`);
 
-  const environment = process.env.ENV;
-  const region = process.env.REGION;
-  const apiGraphQLAPIIdOutput = process.env.API_ALUMNIFRONTEND_GRAPHQLAPIIDOUTPUT;
-  const userTableName = `User-${apiGraphQLAPIIdOutput}-${environment}`;
+  const variables = {
+    email: event.request.userAttributes.email,
+    name: event.request.userAttributes.name,
+    family_name: event.request.userAttributes.family_name,
+    owner: `${event.request.userAttributes.sub}::${event.userName}`
+  };
 
-  console.log('Region: ', region);
-  console.log('Table: ', userTableName);
+  const endpoint = new URL(GRAPHQL_ENDPOINT);
 
-  AWS.config.update({ region: region });
+  const signer = new SignatureV4({
+    credentials: defaultProvider(),
+    region: AWS_REGION,
+    service: 'appsync',
+    sha256: Sha256
+  });
 
-  const date = new Date();
+  const requestToBeSigned = new HttpRequest({
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      host: endpoint.host
+    },
+    hostname: endpoint.host,
+    body: JSON.stringify({ query, variables, authMode: 'AWS_IAM' }),
+    path: endpoint.pathname
+  });
 
-  if (event.request.userAttributes.sub) {
-    const ddbParams = {
-      Item: {
-        id: { S: event.request.userAttributes.sub },
-        __typename: { S: 'User' },
-        name: { S: event.request.userAttributes.name },
-        family_name: { S: event.request.userAttributes.family_name },
-        email: { S: event.request.userAttributes.email },
-        createdAt: { S: date.toISOString() },
-        updatedAt: { S: date.toISOString() }
-      },
-      TableName: userTableName
+  const signed = await signer.sign(requestToBeSigned);
+  const request = new Request(endpoint, signed);
+
+  let statusCode = 200;
+  let body;
+  let response;
+
+  try {
+    response = await fetch(request);
+    body = await response.json();
+    if (body.errors) statusCode = 400;
+  } catch (error) {
+    statusCode = 500;
+    body = {
+      errors: [
+        {
+          message: error.message
+        }
+      ]
     };
-
-    try {
-      await ddb.putItem(ddbParams).promise();
-      console.log('Success');
-    } catch (err) {
-      console.log('Error', err);
-    }
-  } else {
-    console.log('Sub does not exist!');
   }
 
-  return event;
+  console.log(`statusCode: ${statusCode}`);
+  console.log(`body: ${JSON.stringify(body)}`);
+
+  return {
+    statusCode,
+    body: JSON.stringify(body)
+  };
 };
