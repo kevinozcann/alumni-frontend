41e314d759a28b5c270acd84ffe6e03fdc567208
Simsek Mert <simsek97@gmail.com>
2023-01-07 15:03:59 -0500

    Update environments

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,73 +1,73 @@
 export type AmplifyDependentResourcesAttributes = {
-  auth: {
-    alumnifrontend88c8d6e7: {
-      IdentityPoolId: 'string';
-      IdentityPoolName: 'string';
-      UserPoolId: 'string';
-      UserPoolArn: 'string';
-      UserPoolName: 'string';
-      AppClientIDWeb: 'string';
-      AppClientID: 'string';
-    };
-    userPoolGroups: {
-      UserGroupRole: 'string';
-      AdminGroupRole: 'string';
-    };
-  };
-  function: {
-    S3Trigger73b92967: {
-      Name: 'string';
-      Arn: 'string';
-      Region: 'string';
-      LambdaExecutionRole: 'string';
-    };
-    AdminQueriesa1643cb4: {
-      Name: 'string';
-      Arn: 'string';
-      Region: 'string';
-      LambdaExecutionRole: 'string';
-    };
-    alumnifrontend88c8d6e7PostAuthentication: {
-      Name: 'string';
-      Arn: 'string';
-      LambdaExecutionRole: 'string';
-      Region: 'string';
-    };
-    alumnifrontend88c8d6e7PostConfirmation: {
-      Name: 'string';
-      Arn: 'string';
-      LambdaExecutionRole: 'string';
-      Region: 'string';
-    };
-    alumnifrontend88c8d6e7PreAuthentication: {
-      Name: 'string';
-      Arn: 'string';
-      LambdaExecutionRole: 'string';
-      Region: 'string';
-    };
-    alumnifrontend88c8d6e7PreSignup: {
-      Name: 'string';
-      Arn: 'string';
-      LambdaExecutionRole: 'string';
-      Region: 'string';
-    };
-  };
-  storage: {
-    storage: {
-      BucketName: 'string';
-      Region: 'string';
-    };
-  };
-  api: {
-    alumnifrontend: {
-      GraphQLAPIKeyOutput: 'string';
-      GraphQLAPIIdOutput: 'string';
-      GraphQLAPIEndpointOutput: 'string';
-    };
-    AdminQueries: {
-      RootUrl: 'string';
-      ApiName: 'string';
-      ApiId: 'string';
-    };
-  };
-};
+    "auth": {
+        "alumnifrontend88c8d6e7": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        },
+        "userPoolGroups": {
+            "UserGroupRole": "string",
+            "AdminGroupRole": "string"
+        }
+    },
+    "function": {
+        "S3Trigger73b92967": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        },
+        "AdminQueriesa1643cb4": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        },
+        "alumnifrontend88c8d6e7PostAuthentication": {
+            "Name": "string",
+            "Arn": "string",
+            "LambdaExecutionRole": "string",
+            "Region": "string"
+        },
+        "alumnifrontend88c8d6e7PostConfirmation": {
+            "Name": "string",
+            "Arn": "string",
+            "LambdaExecutionRole": "string",
+            "Region": "string"
+        },
+        "alumnifrontend88c8d6e7PreAuthentication": {
+            "Name": "string",
+            "Arn": "string",
+            "LambdaExecutionRole": "string",
+            "Region": "string"
+        },
+        "alumnifrontend88c8d6e7PreSignup": {
+            "Name": "string",
+            "Arn": "string",
+            "LambdaExecutionRole": "string",
+            "Region": "string"
+        }
+    },
+    "storage": {
+        "storage": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    },
+    "api": {
+        "alumnifrontend": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        },
+        "AdminQueries": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
